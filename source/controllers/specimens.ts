871d6ef72de986bba17d2f2a33f084fdccaa1b70
Mats Jacobsson <mats.jacobsson@qlik.com>
2022-07-26 11:28:24 +0200

    Update to specimens

--- source/controllers/specimens.ts
+++ source/controllers/specimens.ts
@@ -1,37 +1,36 @@
 import { Request, Response, NextFunction } from 'express';
 import axios, { AxiosResponse } from 'axios';
 
-interface Post {
-    userId: Number;
+interface Specimen {
     id: Number;
-    title: String;
-    body: String;
+    scientificName: String;
+    commonName: String;
 }
 
 const getSpecimens = async (req: Request, res: Response, next: NextFunction) => {
     let result: AxiosResponse = await axios.get(`https://mockend.com/mjacobsson/herpmanager-api/specimens`);
-    let posts: [Post] = result.data;
+    let specimens: [Specimen] = result.data;
     return res.status(200).json({
-        message: posts
+        message: specimens
     });
 };
 
 const getSpecimen = async (req: Request, res: Response, next: NextFunction) => {
     let id: string = req.params.id;
     let result: AxiosResponse = await axios.get(`https://mockend.com/mjacobsson/herpmanager-api/specimens/${id}`);
-    let post: Post = result.data;
+    let specimen: Specimen = result.data;
     return res.status(200).json({
-        message: post
+        message: specimen
     });
 };
 
 const updateSpecimen = async (req: Request, res: Response, next: NextFunction) => {
     let id: string = req.params.id;
-    let title: string = req.body.title ?? null;
-    let body: string = req.body.body ?? null;
+    let scientificName: string = req.body.scientificName ?? null;
+    let commonName: string = req.body.commonName ?? null;
     let response: AxiosResponse = await axios.put(`https://mockend.com/mjacobsson/herpmanager-api/specimens/${id}`, {
-        ...(title && { title }),
-        ...(body && { body })
+        ...(scientificName && { scientificName }),
+        ...(commonName && { commonName })
     });
     return res.status(200).json({
         message: response.data
@@ -47,11 +46,13 @@ const deleteSpecimen = async (req: Request, res: Response, next: NextFunction) =
 };
 
 const addSpecimen = async (req: Request, res: Response, next: NextFunction) => {
-    let title: string = req.body.title;
-    let body: string = req.body.body;
+    let id: string = req.body.id;
+    let scientificName: string = req.body.scientificName;
+    let commonName: string = req.body.commonName
     let response: AxiosResponse = await axios.post(`https://mockend.com/mjacobsson/herpmanager-api/specimens`, {
-        title,
-        body
+        id,
+        scientificName,
+        commonName
     });
     return res.status(200).json({
         message: response.data
