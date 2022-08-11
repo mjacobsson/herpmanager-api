9933c165684b84093a1df2133942da52a4e0b8e8
Mats Jacobsson <mats.jacobsson@qlik.com>
2022-08-11 19:22:50 +0200

    Basic functionality for specimens and breeding events.

--- source/controllers/breeding-events.ts
+++ source/controllers/breeding-events.ts
@@ -1,20 +1,91 @@
-import { Specimen } from "./specimens";
 import { Request, Response, NextFunction } from 'express';
-import axios, { AxiosResponse } from 'axios';
+import { Schema, model } from 'mongoose';
 
-export interface BreedingEvent {
+export interface IBreedingEvent {
+    id: Number;
     date: Date;
-    male: Specimen;
-    female: Specimen;
+    male: String;
+    female: String;
     comment: String;
 }
 
-const getBreedingEvents = async (req: Request, res: Response, next: NextFunction) => {
-    let result: AxiosResponse = await axios.get(`https://mockend.com/mjacobsson/herpmanager-api/breedings`);
-    let breedings: [BreedingEvent] = result.data;
-    return res.status(200).json({
-        message: breedings
-    });
+const breedingEventSchema = new Schema<IBreedingEvent>({
+    id: {type: Number, required: true, unique: true},
+    date: {type: Date, required: true},
+    male: {type: String, required: false},
+    female: {type: String, required: false},
+    comment: {type: String, required: true}
+});
+
+const breedingEvent = model<IBreedingEvent>('BreedingEvent', breedingEventSchema);
+
+const addBreedingEvent = async (req: Request, res: Response, next: NextFunction) => {
+    let id: Number = req.body.id;
+    let date: Date = req.body.date;
+    let male: String = req.body.male;
+    let female: String = req.body.female;
+    let comment: String = req.body.comment;
+
+    try {
+        const result = await breedingEvent.create({
+            id,
+            date,
+            male,
+            female,
+            comment
+        });
+        return res.status(200).json(result);
+    }  catch (e: any) {
+        if(e.code == 11000) {
+            return res.status(403).json(e.message);
+        } else {
+            return res.status(500).json(e.message);  
+        }
+    }
+
 };
 
-export default { getBreedingEvents }
+const getBreedingEvents = async (req: Request, res: Response, next: NextFunction) => {
+    const result = await breedingEvent.find();
+    return res.status(result.length != 0 ? 200 : 404).json(result);
+};
+
+const getBreedingEvent = async (req: Request, res: Response, next: NextFunction) => {
+    let id: String = req.params.id;
+    const result = await breedingEvent.findOne({id: id});
+    return res.status(result ? 200 : 404).json(result);
+};
+
+const updateBreedingEvent = async (req: Request, res: Response, next: NextFunction) => {
+    let id: string = req.params.id;
+    let date: string = req.body.date;
+    let male: string = req.body.male;
+    let female: string = req.body.female;
+    
+    const result = await breedingEvent.updateOne(
+        {
+            id: id, 
+            date: date, 
+            male: male, 
+            female: female
+        });
+    return res.status(200).json(result);
+};
+
+const deleteBreedingEvent = async (req: Request, res: Response, next: NextFunction) => {
+    let id: string = req.params.id;
+    const {acknowledged, deletedCount} = await breedingEvent.deleteOne({id: id});
+
+    if (acknowledged ) {
+        if (deletedCount == 1) {
+            return res.status(200).json('breeding event deleted successfully');
+        } else if (deletedCount == 0) {
+            return res.status(404).json('breeding event not found');     
+        } else {
+            return res.status(500).json('something unexpected happend while trying to delete');  
+        }
+    }
+    return res.status(500).json('Something went wrong');
+};
+
+export default { addBreedingEvent, getBreedingEvents, getBreedingEvent, updateBreedingEvent, deleteBreedingEvent }
---
+++ tests/db.ts
@@ -0,0 +1,20 @@
+import mongoose  from 'mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+
+let mongo: MongoMemoryServer;
+
+const connectDB = async () => {
+    mongo = await MongoMemoryServer.create();
+    const uri = mongo.getUri('herpmanager');
+    console.log(`Mongo in-memory server URI: ${uri}`);
+
+    await mongoose.connect(uri, {dbName: "TESTDB"});
+}
+
+const closeDB = async () => {
+    await mongoose.connection.dropDatabase();
+    await mongoose.connection.close();
+    await mongo.stop();
+}
+
+export { connectDB, closeDB }
