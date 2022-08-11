a89aca825923df6bc05ff32220da27afddda1243
Mats Jacobsson <mats.jacobsson@qlik.com>
2022-08-11 23:18:20 +0200

    Add editorconfig, eslint and prettier

--- source/controllers/breeding-events.ts
+++ source/controllers/breeding-events.ts
@@ -2,90 +2,121 @@ import { Request, Response, NextFunction } from 'express';
 import { Schema, model } from 'mongoose';
 
 export interface IBreedingEvent {
-    id: Number;
-    date: Date;
-    male: String;
-    female: String;
-    comment: String;
+  id: number;
+  date: Date;
+  male: string;
+  female: string;
+  comment: string;
 }
 
 const breedingEventSchema = new Schema<IBreedingEvent>({
-    id: {type: Number, required: true, unique: true},
-    date: {type: Date, required: true},
-    male: {type: String, required: false},
-    female: {type: String, required: false},
-    comment: {type: String, required: true}
+  id: { type: Number, required: true, unique: true },
+  date: { type: Date, required: true },
+  male: { type: String, required: false },
+  female: { type: String, required: false },
+  comment: { type: String, required: true }
 });
 
-const breedingEvent = model<IBreedingEvent>('BreedingEvent', breedingEventSchema);
+const breedingEvent = model<IBreedingEvent>(
+  'BreedingEvent',
+  breedingEventSchema
+);
 
-const addBreedingEvent = async (req: Request, res: Response, next: NextFunction) => {
-    let id: Number = req.body.id;
-    let date: Date = req.body.date;
-    let male: String = req.body.male;
-    let female: String = req.body.female;
-    let comment: String = req.body.comment;
+const addBreedingEvent = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const id: number = req.body.id;
+  const date: Date = req.body.date;
+  const male: string = req.body.male;
+  const female: string = req.body.female;
+  const comment: string = req.body.comment;
 
-    try {
-        const result = await breedingEvent.create({
-            id,
-            date,
-            male,
-            female,
-            comment
-        });
-        return res.status(200).json(result);
-    }  catch (e: any) {
-        if(e.code == 11000) {
-            return res.status(403).json(e.message);
-        } else {
-            return res.status(500).json(e.message);  
-        }
-    }
-
-};
-
-const getBreedingEvents = async (req: Request, res: Response, next: NextFunction) => {
-    const result = await breedingEvent.find();
-    return res.status(result.length != 0 ? 200 : 404).json(result);
-};
-
-const getBreedingEvent = async (req: Request, res: Response, next: NextFunction) => {
-    let id: String = req.params.id;
-    const result = await breedingEvent.findOne({id: id});
-    return res.status(result ? 200 : 404).json(result);
-};
-
-const updateBreedingEvent = async (req: Request, res: Response, next: NextFunction) => {
-    let id: string = req.params.id;
-    let date: string = req.body.date;
-    let male: string = req.body.male;
-    let female: string = req.body.female;
-    
-    const result = await breedingEvent.updateOne(
-        {
-            id: id, 
-            date: date, 
-            male: male, 
-            female: female
-        });
+  try {
+    const result = await breedingEvent.create({
+      id,
+      date,
+      male,
+      female,
+      comment
+    });
     return res.status(200).json(result);
-};
-
-const deleteBreedingEvent = async (req: Request, res: Response, next: NextFunction) => {
-    let id: string = req.params.id;
-    const {acknowledged, deletedCount} = await breedingEvent.deleteOne({id: id});
-
-    if (acknowledged ) {
-        if (deletedCount == 1) {
-            return res.status(200).json('breeding event deleted successfully');
-        } else if (deletedCount == 0) {
-            return res.status(404).json('breeding event not found');     
-        } else {
-            return res.status(500).json('something unexpected happend while trying to delete');  
-        }
+  } catch (e: any) {
+    if (e.code == 11000) {
+      return res.status(403).json(e.message);
+    } else {
+      return res.status(500).json(e.message);
     }
-    return res.status(500).json('Something went wrong');
+  }
 };
 
-export default { addBreedingEvent, getBreedingEvents, getBreedingEvent, updateBreedingEvent, deleteBreedingEvent }
+const getBreedingEvents = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const result = await breedingEvent.find();
+  return res.status(result.length != 0 ? 200 : 404).json(result);
+};
+
+const getBreedingEvent = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const id: string = req.params.id;
+  const result = await breedingEvent.findOne({ id: id });
+  return res.status(result ? 200 : 404).json(result);
+};
+
+const updateBreedingEvent = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const id: string = req.params.id;
+  const date: string = req.body.date;
+  const male: string = req.body.male;
+  const female: string = req.body.female;
+
+  const result = await breedingEvent.updateOne({
+    id: id,
+    date: date,
+    male: male,
+    female: female
+  });
+  return res.status(200).json(result);
+};
+
+const deleteBreedingEvent = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const id: string = req.params.id;
+  const { acknowledged, deletedCount } = await breedingEvent.deleteOne({
+    id: id
+  });
+
+  if (acknowledged) {
+    if (deletedCount == 1) {
+      return res.status(200).json('breeding event deleted successfully');
+    } else if (deletedCount == 0) {
+      return res.status(404).json('breeding event not found');
+    } else {
+      return res
+        .status(500)
+        .json('something unexpected happend while trying to delete');
+    }
+  }
+  return res.status(500).json('Something went wrong');
+};
+
+export default {
+  addBreedingEvent,
+  getBreedingEvents,
+  getBreedingEvent,
+  updateBreedingEvent,
+  deleteBreedingEvent
+};
--- source/controllers/specimens.ts
+++ source/controllers/specimens.ts
@@ -2,109 +2,122 @@ import { Request, Response, NextFunction } from 'express';
 import { Schema, model } from 'mongoose';
 
 export interface ISpecimen {
-    id: String;
-    scientificName: String;
-    commonName: String;
-    sex: String;
-    birthDate: Date;
-    sire: String;
-    dam: String;
+  id: string;
+  scientificName: string;
+  commonName: string;
+  sex: string;
+  birthDate: Date;
+  sire: string;
+  dam: string;
 }
 
-const specimenSchema = new Schema<ISpecimen>(
-    {
-        id: {type: String, required: true, unique: true},
-        scientificName: {type: String, required: true},
-        commonName: {type: String, rquired: true},
-        sex: {type: String, required: true},
-        birthDate: {type: Date, required: false},
-        sire: {type: String, required: false},
-        dam: {type: String, required: false}
-
-    }
-);
+const specimenSchema = new Schema<ISpecimen>({
+  id: { type: String, required: true, unique: true },
+  scientificName: { type: String, required: true },
+  commonName: { type: String, rquired: true },
+  sex: { type: String, required: true },
+  birthDate: { type: Date, required: false },
+  sire: { type: String, required: false },
+  dam: { type: String, required: false }
+});
 
 const specimen = model<ISpecimen>('Specimen', specimenSchema);
 
 const addSpecimen = async (req: Request, res: Response, next: NextFunction) => {
-    let id: string = req.body.id;
-    let scientificName: string = req.body.scientificName;
-    let commonName: string = req.body.commonName;
-    let sex: string = req.body.sex;
-    let birthDate: Date = req.body.birthDate;
-    let sire: String = req.body.sire;
-    let dam: String = req.body.dam;
+  const id: string = req.body.id;
+  const scientificName: string = req.body.scientificName;
+  const commonName: string = req.body.commonName;
+  const sex: string = req.body.sex;
+  const birthDate: Date = req.body.birthDate;
+  const sire: string = req.body.sire;
+  const dam: string = req.body.dam;
 
-    try {
-        const result = await specimen.create({
-            id,
-            scientificName,
-            commonName,
-            sex,
-            birthDate,
-            sire,
-            dam
-        });
-        return res.status(200).json(result);
-    } catch (e: any) {
-        if(e.code == 11000) {
-            return res.status(403).json(e.message);
-        } else {
-            return res.status(500).json(e.message);  
-        }
+  try {
+    const result = await specimen.create({
+      id,
+      scientificName,
+      commonName,
+      sex,
+      birthDate,
+      sire,
+      dam
+    });
+    return res.status(200).json(result);
+  } catch (e: any) {
+    if (e.code == 11000) {
+      return res.status(403).json(e.message);
+    } else {
+      return res.status(500).json(e.message);
     }
+  }
 };
 
-const getSpecimens = async (req: Request, res: Response, next: NextFunction) => {
-    const result = await specimen.find();
-    return res.status(result.length != 0 ? 200 : 404).json(result);
+const getSpecimens = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const result = await specimen.find();
+  return res.status(result.length != 0 ? 200 : 404).json(result);
 };
 
 const getSpecimen = async (req: Request, res: Response, next: NextFunction) => {
-    let id: string = req.params.id;
-    const result = await specimen.findOne({id: id});
-    return res.status(result ? 200 : 404).json(result);
+  const id: string = req.params.id;
+  const result = await specimen.findOne({ id: id });
+  return res.status(result ? 200 : 404).json(result);
 };
 
-const updateSpecimen = async (req: Request, res: Response, next: NextFunction) => {
-    let id: string = req.params.id;
-    let scientificName: string = req.body.scientificName;
-    let commonName: string = req.body.commonName;
-    let sex: string = req.body.sex;
-    let birthDate: string = req.body.birthDate;
-    let sire: string = req.body.sire;
-    let dam: string = req.body.dam;
-    
-    const response = await specimen.updateOne(
-        {
-            id: id, 
-            scientificName: scientificName, 
-            commonName: commonName, 
-            sex: sex, 
-            birthDate: birthDate, 
-            sire: sire, 
-            dam: dam
-        });
-    return res.status(200).json(response);
+const updateSpecimen = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const id: string = req.params.id;
+  const scientificName: string = req.body.scientificName;
+  const commonName: string = req.body.commonName;
+  const sex: string = req.body.sex;
+  const birthDate: string = req.body.birthDate;
+  const sire: string = req.body.sire;
+  const dam: string = req.body.dam;
+
+  const response = await specimen.updateOne({
+    id: id,
+    scientificName: scientificName,
+    commonName: commonName,
+    sex: sex,
+    birthDate: birthDate,
+    sire: sire,
+    dam: dam
+  });
+  return res.status(200).json(response);
 };
 
-const deleteSpecimen = async (req: Request, res: Response, next: NextFunction) => {
-    let id: string = req.params.id;
-    const {acknowledged, deletedCount} = await specimen.deleteOne({id: id});
+const deleteSpecimen = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const id: string = req.params.id;
+  const { acknowledged, deletedCount } = await specimen.deleteOne({ id: id });
 
-    if (acknowledged ) {
-        if (deletedCount == 1) {
-            return res.status(200).json('specimen deleted successfully');
-        } else if (deletedCount == 0) {
-            return res.status(404).json('specimen not found');     
-        } else {
-            return res.status(500).json('something unexpected happend while trying to delete');  
-        }
-
+  if (acknowledged) {
+    if (deletedCount == 1) {
+      return res.status(200).json('specimen deleted successfully');
+    } else if (deletedCount == 0) {
+      return res.status(404).json('specimen not found');
+    } else {
+      return res
+        .status(500)
+        .json('something unexpected happend while trying to delete');
     }
-    return res.status(500).json('Something went wrong');
+  }
+  return res.status(500).json('Something went wrong');
 };
 
-
-
-export default { addSpecimen, getSpecimens, getSpecimen, updateSpecimen, deleteSpecimen  };
+export default {
+  addSpecimen,
+  getSpecimens,
+  getSpecimen,
+  updateSpecimen,
+  deleteSpecimen
+};
